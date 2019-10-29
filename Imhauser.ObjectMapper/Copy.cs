80d04b999465bf6e65bf9530c8ea5680141eaa28
Eric Imhauser <eric@imhauser.com>
2019-10-29 14:16:19 +0100

    Version 1.0.1 + Unit Tests

---
+++ Imhauser.ObjectMapper/Copy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Imhauser.ObjectMapper
+{
+    /// <summary>
+    /// Static class implementing copy methods of the Imhauser.ObjectMapper
+    /// </summary>
+    public static class Copy
+    {
+        /// <summary>
+        /// Copy all common properties from a source object to a new target object<br/>
+        /// Only properties with same name and type are copied
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source object</typeparam>
+        /// <typeparam name="TTarget">Type of the target object</typeparam>
+        /// <param name="source">Source object</param>
+        /// <returns>The newly creadted target object</returns>
+        public static TTarget To<TSource,TTarget>(TSource source) where TSource : class
+        {
+            TTarget target = (TTarget)Activator.CreateInstance(typeof(TTarget));
+            return To<TSource, TTarget>(source, target);
+        }
+
+        /// <summary>
+        /// Copy all common properties from a source object to an existing target object.<br/>
+        /// Only properties with same name and type are copied
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source object</typeparam>
+        /// <typeparam name="TTarget">Type of the target object</typeparam>
+        /// <param name="source">Source object</param>
+        /// <param name="target">Target object</param>
+        /// <returns>The target object</returns>
+        public static TTarget To<TSource, TTarget>(TSource source, TTarget target) where TSource : class
+        {
+            var sourceProperties = source.GetType().GetProperties();
+            var targetProperties = target.GetType().GetProperties();
+
+            foreach (var parentProperty in sourceProperties)
+            {
+                foreach (var childProperty in targetProperties)
+                {
+                    if (parentProperty.Name == childProperty.Name && parentProperty.PropertyType == childProperty.PropertyType)
+                    {
+                        childProperty.SetValue(target, parentProperty.GetValue(source));
+                        break;
+                    }
+                }
+            }
+            return target;
+        }
+
+        /// <summary>
+        /// Copy all common properties from a list of source objects to new list of objects with the target type.<br/>
+        /// Only properties with same name and type are copied
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source object</typeparam>
+        /// <typeparam name="TTarget">Type of the target object</typeparam>
+        /// <param name="source">IEnumerable list of source objects</param>
+        /// <returns>IEnumerable list of target objects</returns>
+        public static IEnumerable<TTarget> ToList<TSource, TTarget>(IEnumerable<TSource> source) where TSource : class
+        {
+            return source.Select(item => ObjectMapper.Copy.To<TSource, TTarget>(item));
+        }
+
+
+    }
+}
---
+++ ObjectMapper.Tests/UnitTest1.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ObjectMapper.Tests
+{
+    public class AddressDb
+    {
+        public int Id{ get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+    public class ContactDb
+    {
+        public string ContactName { get; set; }
+        public string Phone { get; set; }
+    }
+
+
+
+    public class View
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string ContactName { get; set; }
+        public string Phone { get; set; }
+    }
+
+
+
+    public class CopyTests
+    {
+        [Fact]
+        public void ToTest()
+        {
+            var addressDb = new AddressDb()
+            {
+                Street = "Ellegårdsvej 74",
+                ZipCode = "2820",
+                City = "Gentofte"
+            };
+            var contactDb = new ContactDb()
+            {
+                ContactName = "Eric Imhauser",
+                Phone = "1523456789",
+            };
+
+            var view = Imhauser.ObjectMapper.Copy.To<AddressDb, View>(addressDb);
+            Assert.Equal(addressDb.Street , view.Street);
+
+            view = Imhauser.ObjectMapper.Copy.To<ContactDb, View>(contactDb, view);
+            Assert.Equal(contactDb.ContactName, view.ContactName);
+        }
+
+        [Fact]
+        public void ToListTest()
+        {
+            List<AddressDb> list = new List<AddressDb>()
+            {
+                new AddressDb()
+                {
+                    Street = "Ellegårdsvej 74",
+                    ZipCode = "2820",
+                    City = "Gentofte"
+                },
+                new AddressDb()
+                {
+                    Street = "CVE Knuthsvej 2D",
+                    ZipCode = "2900",
+                    City = "Hellerup"
+                }
+            };
+
+            var targetList = Imhauser.ObjectMapper.Copy.ToList<AddressDb, View>(list).ToList();
+            Assert.Equal(list[0].Street, targetList[0].Street);
+        }
+    }
+}
